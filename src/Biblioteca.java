ac633e77cbee7ee1efb999460d00ae2afd77ec03
Lorenzo <lorenzo.chieppino@gmail.com>
2026-03-27 11:17:01 +0100

    classe Biblioteca con le seguenti strutture dati:
    List<Libro<?>> → Memorizza tutti i libri disponibili
    Set<String> → Contiene utenti registrati
    Map<String, Stack<Libro<?>>> → Mappa un utente alla sua pila di libri presi in prestito
    Queue<Libro<?>> → Coda di libri in attesa di essere restituiti
    
    Seguono:
    
    crezione metodi
    
    aggiungiLibro();
    rimuoviiLibro();
    registraUtente();
    prestaLibro();
    restituisciLibro();

---
+++ src/Biblioteca.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Biblioteca {
+
+
+    List<Libro<?>> libreria = new ArrayList<>();
+    Set<String> utenti = new HashSet<>();
+    Map<String, Stack<Libro<?>>> prestiti = new HashMap<>();
+    Queue<Libro<?>> libriDaRestituire = new LinkedList<>();
+
+    public void aggiungiLibro(Libro<?> libro) {
+        libreria.add(libro);
+    }
+
+    public <T> void rimuoviLibro(T id) {
+        for (Libro<?> libro : libreria) {
+            if (libro.getIsbn().equals(id)) {
+                libreria.remove(libro);
+            }
+        }
+    }
+    public void registraUtente(String nome){
+        utenti.add(nome);
+    }
+
+    public void prestaLibro(String utente, Libro<?> libro){
+        if (prestiti.containsKey(utente)== false){
+            prestiti.put(utente, new Stack<>());
+            prestiti.get(utente).push(libro);
+            libreria.remove(libro);
+
+
+        }
+
+    }
+    public void restituisciLibro(String utente){
+
+        if (prestiti.containsKey(utente)){
+            libriDaRestituire.offer(prestiti.get(utente).pop());
+        }
+    }
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
---
+++ src/Libro.java
@@ -0,0 +1,13 @@
+public class Libro <T>{
+
+    private T isbn;
+
+    public void setIsbn(T isbn) {
+        this.isbn = isbn;
+    }
+
+    public T getIsbn() {
+        return isbn;
+    }
+}
+
