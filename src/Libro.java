3197b83bc11d0da357079a40ca157724e6d8cb83
Lorenzo <lorenzo.chieppino@gmail.com>
2026-03-27 13:37:07 +0100

    Creazione Classe Libro <T> con attributi titolo e isbn costruttore e metodi getter setter

--- src/Libro.java
+++ src/Libro.java
@@ -1,13 +1,31 @@
 public class Libro <T>{
 
     private T isbn;
+    private String titolo;
 
-    public void setIsbn(T isbn) {
+
+    public Libro (String titolo, T isbn){
+        this.titolo=titolo;
+        this.isbn=isbn;}
+
+
+
+    public void setTitolo (){
+        this.titolo= titolo;}
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setIsbn() {
         this.isbn = isbn;
     }
 
     public T getIsbn() {
         return isbn;
     }
-}
+
+    }
+
+
 
